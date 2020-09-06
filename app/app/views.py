d1ab7346c7528e30731094ee1539e45448e426fa
Megan Thong <meganthong@outlook.com>
2020-09-06 14:08:31 +0800

    basic backend

---
+++ app/app/views.py
@@ -0,0 +1,24 @@
+from app import app
+from flask import render_template
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/register")
+def register():
+    return render_template("register.html")
+
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+@app.route("/home")
+def home():
+    return render_template("home.html")
+
